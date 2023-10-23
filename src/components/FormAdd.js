d318b3fd57951093aa007d826805f2871272198d
HirdyanandGour <skithngour@gmail.com>
2023-10-23 12:01:05 +0530

    validation added on form add

--- src/components/FormAdd.js
+++ src/components/FormAdd.js
@@ -6,6 +6,7 @@ import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Stack from '@mui/material/Stack';
 import Alert from '@mui/material/Alert';
+import AlertTitle from '@mui/material/AlertTitle';
   
 export default function FormAdd() {
 	const [ raid, setRaid ] =  useState('');
@@ -15,27 +16,32 @@ export default function FormAdd() {
 	const [ mobile, setMobile ] =  useState('');
 	const [ open, setOpen ] = useState(false);
 	const [message, setMessage] = useState('');
+	const [ errorm, setErrorm] = useState(false);
 	const saveTodo = async (event) => {
 		try{
 			event.preventDefault();
-			// Call your API here
-			const response = await fetch('/api/postData', {
-				method: 'POST',
-				headers: {
-				'Content-Type': 'application/json',
-				},
-				body: JSON.stringify({ raid, name, mname, fname, mobile }),
-			});
-			if (response.ok === true) {
-				setOpen(true);
-				setRaid("");
-				setName("");
-				setMname("");
-				setFname("");
-				setMobile("");
-				setMessage("Data added suuccessfully")
+			if( raid != '' || name != '' ){
+				const response = await fetch('/api/postData', {
+					method: 'POST',
+					headers: {
+					'Content-Type': 'application/json',
+					},
+					body: JSON.stringify({ raid, name, mname, fname, mobile }),
+				});
+				if (response.ok === true) {
+					setOpen(true);
+					setRaid("");
+					setName("");
+					setMname("");
+					setFname("");
+					setMobile("");
+					setMessage("Data added suuccessfully")
+				}else{
+					throw new Error('Failed to add data')
+				}
 			}else{
-				throw new Error('Failed to add data')
+				setErrorm(true)
+				setMessage("Ration card no and Name can not be empty. !!")
 			}
 		}catch(e){
 			console.error("Error adding document")
@@ -48,13 +54,15 @@ export default function FormAdd() {
 			<Box sx={{ width: 500, maxWidth: '100%', display: 'table', margin: '0 auto', padding: '10ch 2ch' }}>
 				<h5>Sahi jankari record me add kare </h5>
 				<Stack spacing={2}>
-					{ (open)? <Alert severity="success" onClose={ () => setOpen({open}) } > { message } </Alert> : ''}
+					
 					<TextField required fullWidth onChange={(e) => setRaid(e.target.value)} label="Ration Card ID e.g. 123456789012" id="raid" value={raid} />
 					<TextField required fullWidth onChange={(e) => setName(e.target.value)}  label="Name e.g. amit" id="name" value={name}/>
 					<TextField fullWidth onChange={(e) => setMname(e.target.value)} label="Mother Name e.g. madhu" id="mother_name" value={mname} />
 					<TextField fullWidth onChange={(e) => setFname(e.target.value)} label="Father Name e.g. sumit" id="father_name" value={fname} />
 					<TextField fullWidth onChange={(e) => setMobile(e.target.value)} label="Mobile e.g. 9876543210" id="mobile" value={mobile} />
 					<Button variant="outlined"  onClick={saveTodo}>Add Data</Button>
+					{ (errorm)? <Alert severity="error"><AlertTitle>Error</AlertTitle><strong>{message}</strong></Alert> : "" }
+					{ (open)? <Alert severity="success" onClose={ () => setOpen({open}) } > { message } </Alert> : ''}
 				</Stack>
 			</Box>
     	</Box>
